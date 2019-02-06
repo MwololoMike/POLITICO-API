86842f4144f7d04cee778f5f6ebd62ee203457f6
MwololoMike <mikemwololo19@gmail.com>
2019-02-07 00:37:48 +0300

    [starts #163736046]Enables a user to view all political offices.

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, make_response, request
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+office_list = []
+
+@app.route('/offices', methods=['GET'])
+def offices():
+    return make_response(jsonify({
+         "status": "200",
+         "offices" : office_list
+    }),200)
+
 
 if __name__ == "__main__":
     app.run()
