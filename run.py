05b6808a64a8b3ad4d84a865c39f569414cb4586
MwololoMike <mikemwololo19@gmail.com>
2019-02-07 11:37:56 +0300

    [starts #163735698]Enables an admin to create a new office.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ party_list = []
 
 
 @app.route('/offices', methods=['GET'])
-def offices():
+def api_offices():
     return make_response(jsonify({
         "status": "200",
         "offices": office_list
@@ -14,28 +14,51 @@ def offices():
 
 
 @app.route('/parties', methods=['GET'])
-def parties():
+def api_parties():
     return make_response(jsonify({
         "status": "200",
         "parties": party_list
     }), 200)
 
 
-@app.route('/offices/ < office-id > ', methods=['GET'])
-def offices():
+@app.route('/offices/<office_id>', methods=['GET'])
+def api_office(office_id):
+    return make_response(jsonify({
+         "status": "200",
+         "offices": office_list[office_id]
+     }), 200)
+
+
+@app.route('/parties/<party_id>', methods=['GET'])
+def api_party(party_id):
     return make_response(jsonify({
         "status": "200",
-        "offices": office_list
+        "parties": party_list[party_id]
     }), 200)
 
 
-@app.route('/parties/<party-id>', methods=['GET'])
-def parties():
-    return make_response(jsonify({
-        "status": "200",
-        "parties": party_list
-    }), 200)
+@app.route('/offices', methods=['POST'])
+def api_add_office():
+    data = request.get_json()
+    name = data["name"]
+    office_id = len(office_list)+1
+    kind = data["type"]
 
+    new_office = {
+        "name": name,
+        "type": kind,
+        "office_id": office_id
+
+    }
+
+    office_list.append(new_office)
+    return make_response(jsonify({
+        "message": "Office created successfully.",
+        "status": "201",
+        "offices": new_office['name'],
+        "office_id": office_id,
+        "type": new_office['type']
+    }), 201)
 
 
 if __name__ == "__main__":
