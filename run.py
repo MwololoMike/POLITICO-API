495fa0e64e0509dde73e78d2540c807d62c57e1c
MwololoMike <mikemwololo19@gmail.com>
2019-02-08 10:54:10 +0300

    [starts #163735152]Enables an admin to edit a party name.

--- run.py
+++ run.py
@@ -1,85 +1,126 @@
 from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
+
 office_list = []
 party_list = []
 
 
 @app.route('/offices', methods=['GET'])
 def api_offices():
-    return make_response(jsonify({
-        "status": "200",
-        "offices": office_list
-    }), 200)
+    if len(office_list) >= 0:
+        return make_response(jsonify({"status": "200", "data": office_list}), 200)
+    else:
+        return make_response(jsonify({"status": "404", "message": "Error!Request not found."}), 404)
 
 
 @app.route('/parties', methods=['GET'])
 def api_parties():
-    return make_response(jsonify({
-        "status": "200",
-        "parties": party_list
-    }), 200)
+    if len(party_list) >= 0:
+        return make_response(jsonify({"status": "200", "data": party_list}), 200)
+    else:
+        return make_response(jsonify({"status": "404", "message": "Error!Request not found."}), 404)
 
 
 @app.route('/offices/<office_id>', methods=['GET'])
 def api_office(office_id):
-    return make_response(jsonify({
-         "status": "200",
-         "offices": office_list[office_id]
-     }), 200)
+    try:
+        office = office_list[int(office_id) - 1]
+        if office:
+            return make_response(jsonify({
+                "status": "200",
+                "office": [office]
+            }), 200)
+        else:
+            return make_response(jsonify({
+                "message": "Invalid Request",
+                "status": "400"
+            }), 400)
+
+    except IndexError:
+        return make_response(jsonify({
+            "message": "Index Error",
+            "status": "400"
+        }), 400)
 
 
 @app.route('/parties/<party_id>', methods=['GET'])
 def api_party(party_id):
-    return make_response(jsonify({
-        "status": "200",
-        "parties": party_list[party_id]
-    }), 200)
+    try:
+        party = party_list[int(party_id) - 1]
+        if party:
+            return make_response(jsonify({
+                "status": "200",
+                "party": [party]
+            }), 200)
+        else:
+            return make_response(jsonify({
+                "message": "Invalid Request",
+                "status": "400"
+            }), 400)
+    except IndexError:
+        return make_response(jsonify({
+            "message": "Index Error",
+            "status": "400"
+        }), 400)
 
 
 @app.route('/offices', methods=['POST'])
 def api_add_office():
     data = request.get_json()
-    name = data["name"]
-    office_id = len(office_list)+1
-    kind = data["type"]
+    office_name = data["name"]
+    if len(office_list) > 0:
+        office_id = office_list[-1]['office_id'] + 1
+    else:
+        office_id = len(office_list) + 1
+    office_type = data['type']
 
     new_office = {
-        "name": name,
-        "type": kind,
-        "office_id": office_id
-
+        "name": office_name,
+        "office_id": office_id,
+        "type": office_type
     }
 
     office_list.append(new_office)
-    return make_response(jsonify({
-        "message": "Office created successfully.",
-        "status": "201",
-        "offices": new_office['name'],
-        "office_id": office_id,
-        "type": new_office['type']
-    }), 201)
+
+    return make_response(jsonify({"message": "Office created successfully.", "status": "201", "data": [new_office]}),
+                         201)
 
 
 @app.route('/parties', methods=['POST'])
 def api_add_party():
     data = request.get_json()
-    name = data["name"]
-    party_id = len(party_list)+1
+    party_name = data["name"]
+    logo_url = data['logoUrl']
+    party_address = data['hqAddress']
+    if len(party_list) > 0:
+        party_id = party_list[-1]['party_id'] + 1
+    else:
+        party_id = len(party_list) + 1
 
     new_party = {
-        "name": name,
-        "party_id": party_id
-
+        "name": party_name,
+        "party_id": party_id,
+        "logoUrl": logo_url,
+        "hqAddress": party_address
     }
+    party_list.append(new_party)
 
-    office_list.append(new_party)
-    return make_response(jsonify({
-        "message": "Party created successfully.",
-        "status": "201",
-        "parties": new_party['name'],
-        "party_id": party_id
-    }), 201)
+    return make_response(jsonify({"message": "Party created successfully.", "status": "201", "data": [new_party]}), 201)
+
+
+@app.route('/parties/<party_id>/name', methods=['PATCH'])
+def api_edit_party(party_id):
+    data = request.get_json()
+    new_party_name = data["name"]
+    for party in party_list:
+        if party['id'] == party_id:
+            party['name'] = new_party_name
+            return make_response(jsonify({
+                "message": "Party name updated successfully", "status": "200", "data": [party]}), 200)
+        else:
+            return make_response(jsonify({"message": "Invalid.", "status": "404"}), 404)
+    return make_response(jsonify({"message": "Error", "status": "404"}), 404)
 
 
 if __name__ == "__main__":
